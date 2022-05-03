dcd61176cbf79664d2a95603528162248ae93ed6
Yao Xie <yao.xie@niceincontact.com>
2022-05-03 18:15:22 -0400

    first draft

---
+++ lambda_function.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def lambda_handler(event, context):
+    d={'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
