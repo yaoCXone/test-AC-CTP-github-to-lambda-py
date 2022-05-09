6d606a50215038d83293b35d1d524e8a3b7a8138
yao xie <yao.xie@niceincontact.com>
2022-05-09 17:04:26 -0400

    commen out access denied code of getting object from s3 bucket.
    waiting for permission role change for access s3 bucket

--- lambda_function.py
+++ lambda_function.py
@@ -9,16 +9,16 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     s3_to_lambda(event, context)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
 def main():   
     d={'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
     print('Done x1')
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
 
 def s3_to_lambda(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
@@ -29,9 +29,10 @@ def s3_to_lambda(event, context):
     print('bucket: '+bucket)
     print('key: '+key)
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        #response = s3.get_object(Bucket=bucket, Key=key)
+        #print("CONTENT TYPE: " + response['ContentType'])
+        print("Received notification of bucket:(" + bucket + ") key:(" +key+") content change.")
+        #return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
