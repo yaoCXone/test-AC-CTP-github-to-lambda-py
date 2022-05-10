7949670fd46f1d382f718ad9702e1af8d69eb25c
Yao Xie <yao.xie@niceincontact.com>
2022-05-10 18:16:00 -0400

    fix lambda function linking

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
 import src.panads_sample as ps
-import src.lambda_handler as lh
+import src.lambda_function as lf
 
 def main():
     ps.print_sample_2x2()
 
 def lambda_handler(event, context):
-    lh.s3_to_lambda(event, context)
+    lf.s3_to_lambda(event, context)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda function!')
