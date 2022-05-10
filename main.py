872b7ac2adfa09357de381a1fd88b6dc92dfe790
Yao Xie <yao.xie@niceincontact.com>
2022-05-10 18:09:06 -0400

    lambda_function not found

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 import src.panads_sample as ps
+import src.lambda_handler as lh
 
 def main():
     ps.print_sample_2x2()
+
+def lambda_handler(event, context):
+    lh.s3_to_lambda(event, context)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda function!')
+    }
 
 
 if __name__ == "__main__":
