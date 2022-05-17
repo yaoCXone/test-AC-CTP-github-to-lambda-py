e1b05a3d0ee54aab2adbfcf1e5610f593823ed85
Yao Xie <yao.xie@niceincontact.com>
2022-05-17 09:24:29 -0400

    Update ctr verify method

--- src/ctr_json.py
+++ src/ctr_json.py
@@ -10,11 +10,13 @@ class ctr_json_object:
 		return ctr_json;
 
 	def is_ctr_json(self):
-		return (self.contains_node("AWSContactTraceRecordFormatVersion") and
-			self.contains_node("AWSAccountId") and
-			self.contains_node("ContactId") and
-			self.contains_node("Recordings"))
+		return (self.contains_key("AWSContactTraceRecordFormatVersion") and
+			self.contains_key("AWSAccountId") and
+			self.contains_key("ContactId") and
+			self.contains_key("Recordings"))
 
-	def contains_node(self, keyword):
+	def contains_key(self, keyword):
 		return not self.ctr_json.get(keyword) is None
+
+
 
